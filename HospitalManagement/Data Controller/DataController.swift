dbd9e33caf0cdbd8cd1801855b7bf87b464c12e7
TARUN KUMAR <130458471+tarunmariyo@users.noreply.github.com>
2025-03-19 22:29:17 +0530

    Data Controller added for super admin, Data model for super admin , hospital detail view added .

---
+++ HospitalManagement/Data Controller/DataController.swift
@@ -0,0 +1,8 @@
+//
+//  DataController.swift
+//  HospitalManagement
+//
+//  Created by Mariyo on 19/03/25.
+//
+
+import Foundation
---
+++ HospitalManagement/Data Model/DataModel.swift
@@ -0,0 +1,8 @@
+//
+//  Data Model.swift
+//  HospitalManagement
+//
+//  Created by Mariyo on 19/03/25.
+//
+
+import Foundation
