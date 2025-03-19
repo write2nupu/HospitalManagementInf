ec3d1e71bd15575dcd6452a67833b5e553e1abe6
TARUN KUMAR <130458471+tarunmariyo@users.noreply.github.com>
2025-03-19 22:31:24 +0530

    data model and data controller added , hospital detail view and dashboard.

--- HospitalManagement/Data Controller/DataController.swift
+++ HospitalManagement/Data Controller/DataController.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import SwiftUI
+@MainActor
+final class HospitalViewModel: ObservableObject {
+    @Published var hospitals: [hospitals] = []
+    
+    func generateRandomPassword() -> String {
+        let digits = "0123456789"
+        return String((0..<6).map { _ in
+            digits.randomElement()!
+        })
+    }
+    
+    func addHospital(_ hospital: hospitals) {
+        hospitals.append(hospital)
+    }
+    
+    func updateHospital(_ hospital: hospitals) {
+        if let index = hospitals.firstIndex(where: { $0.id == hospital.id }) {
+            hospitals[index] = hospital
+        }
+    }
+}
